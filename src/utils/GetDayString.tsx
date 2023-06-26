f31c7408cc9e43a70c8703f273fb824fc67859dc
Meng Yap <tan.meng.yap@pingspace.co>
2023-06-26 21:27:55 +0800

    fix: middle string error

--- src/utils/GetDayString.tsx
+++ src/utils/GetDayString.tsx
@@ -9,7 +9,7 @@ export const getMiddleString = (days: number): string => {
     if (days == 0) return 'is'
     else if (days == -1) return 'is'
     else if (days == 1) return 'is'
-    else if (days < 0) return 'days until'
-    else if (days > 0) return 'days since'
+    else if (days > 0) return 'days until'
+    else if (days < 0) return 'days since'
     else return 'days'
 }
