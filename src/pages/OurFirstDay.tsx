4f8fcb1c6d8776703908f3b43510be114bea684c
tmy010 <136803715+tmy010@users.noreply.github.com>
2023-06-30 18:04:00 +0800

    Update OurFirstDay.tsx

--- src/pages/OurFirstDay.tsx
+++ src/pages/OurFirstDay.tsx
@@ -31,7 +31,7 @@ const OurFirstDay = () => {
             <>
                 <div className="flex h-full w-full items-center justify-center pt-24">
                     <AnimatedElementLayout
-                        nextPath="OurCameronTrip"
+                        nextPath="OurKLTrip"
                         prevPath="OurFirstDate"
                     >
                         <>
